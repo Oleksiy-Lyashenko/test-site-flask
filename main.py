41ba6e024fb8f2f80d0680473a6d4210d53f1e3f
oleksii_lyashenko <oleksij.layshenko@gmail.com>
2025-02-05 16:39:44 +0200

    Change db setting to app

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///posts.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///posts.db') + "?sslmode=require"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
